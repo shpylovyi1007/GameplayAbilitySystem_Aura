42eda0a634be2b35726b8df3f1ad3a4f0e54880e
Yevhenii Shpylovyi <yevheniishpylovyi398@gmail.com>
2026-07-30 19:05:34 +0300

    Animations blueprints and add movement input

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -4,19 +4,17 @@
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
 
-UCLASS()
+UCLASS(Abstract)
 class AURA_API AAuraCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
 	AAuraCharacterBase();
-    virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
 protected:
 	virtual void BeginPlay() override;
 
-	
-
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 };
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
