a43c08f6dff2bca8af4c6c6484cc84bee75dd263
Yevhenii Shpylovyi <yevheniishpylovyi398@gmail.com>
2026-09-05 16:30:17 +0300

    init vital attributes

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -33,6 +33,9 @@ protected:
 	virtual void InitAbilityActorInfo();
 	
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Attributes")
+	TSubclassOf<UGameplayEffect> DefaultVitalAttributes;
+	
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Attributes")
 	TSubclassOf<UGameplayEffect> DefaultPrimaryAttributes;
 	
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Attributes")
